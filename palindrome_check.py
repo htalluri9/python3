935eeff2835dbfb32c7bfcb135f893550a64c7bd
talluri <harishtalluri@gmail.com>
2024-05-21 01:16:15 -0500

    first commit

---
+++ palindrome_check.py
@@ -0,0 +1,14 @@
+s = input("enter the word to check if its palindrome: ")
+def is_palindrome(s):
+    i = 0
+    j = len(s) - 1
+    while i < j:
+        if s[i] != s[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
+
+print(is_palindrome(s))
+
+    
